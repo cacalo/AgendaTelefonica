21d89f8100c528e327464a6c87eb63d663e179b6
cacalo <cacalo219@hotmail.com>
2022-09-13 12:40:46 -0300

    Creación de páginas básicas y configuración de routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: ()=> import('./public/pages/contacts/contacts.module').then(m => m.ContactsModule)
+  },
+  {
+    path: 'login',
+    loadChildren: ()=> import('./public/pages/login/login.module').then(m => m.LoginModule)
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
