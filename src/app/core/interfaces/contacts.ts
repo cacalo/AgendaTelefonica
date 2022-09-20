439a8aa67460e36f3d0ff8a34b618f8ca38e269f
cacalo <cacalo219@hotmail.com>
2022-09-20 19:52:20 -0300

    Interfaz contactos JsonPlaceholder agregada

--- src/app/core/interfaces/contacts.ts
+++ src/app/core/interfaces/contacts.ts
@@ -9,3 +9,35 @@ export const defaultContact:Contact = {
     lastName: "Last Name",
     phoneNumber: "+5412345678"
 }
+
+
+
+export interface Geo {
+    lat: string;
+    lng: string;
+}
+
+export interface Address {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: Geo;
+}
+
+export interface Company {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+}
+
+export interface ContactJsonPlaceholder {
+    id?: number;
+    name?: string;
+    username?: string;
+    email?: string;
+    address?: Address;
+    phone?: string;
+    website?: string;
+    company?: Company;
+}
