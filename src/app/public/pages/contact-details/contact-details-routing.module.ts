5bff45396a577b2ce93f80712b1354222d1d458d
Gonzalo Bechara <60068962+cacalo@users.noreply.github.com>
2022-09-21 10:32:28 -0300

    Redireción de /contacts a home

--- src/app/public/pages/contact-details/contact-details-routing.module.ts
+++ src/app/public/pages/contact-details/contact-details-routing.module.ts
@@ -2,17 +2,24 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ContactDetailsComponent } from './contact-details.component';
 
-const routes: Routes = [{
-  path: 'new',
-  component: ContactDetailsComponent
-},{
-  path: ':id',
-  component: ContactDetailsComponent
-}
-  ]
+const routes: Routes = [
+  {
+    path: 'new',
+    component: ContactDetailsComponent,
+  },
+  {
+    path: ':id',
+    component: ContactDetailsComponent,
+  },
+  {
+    path: '',
+    redirectTo: '/',
+    pathMatch: 'full',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class ContactDetailsRoutingModule { }
+export class ContactDetailsRoutingModule {}
