e3620888391c57bcb6138dc32fd9854973a89033
Gonzalo Bechara <60068962+cacalo@users.noreply.github.com>
2022-10-11 15:35:36 -0300

    Angular forms en Login agregado

--- src/app/public/pages/login/login.component.ts
+++ src/app/public/pages/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { iAuthRequest } from 'src/app/core/interfaces/auth';
+import { AuthService } from 'src/app/core/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,10 +11,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent {
 
-  constructor() { }
+  constructor(private auth:AuthService, private router:Router) { }
 
-  login(){
-    console.log("Login")
+  //Hecho usando NgModel
+  authData:iAuthRequest = {
+    userName : "",
+    password : ""
+  };
+
+
+  async login(form:NgForm){
+    //Valor del formulario para no usar NgModel
+    console.log(form.value);
+    const token = await this.auth.login(form.value);
+    if(token) this.router.navigate(['']);
   }
 
 }
